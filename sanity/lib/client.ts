2100dbc4068cb13b2544dcbed901be67b9ab619a
Himanshu Jha <90416078+HIMANSHU-1504@users.noreply.github.com>
2023-08-13 16:07:37 +0530

    Update client.ts

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,6 +7,4 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
-  token:
-  "skbUhii73SPk5oRzA6z1mzJgrbMg7RJBno2ubvuLjwIiXvKoBUmzAxcU3SdYJUIp1T720GzPLJoQMHuV5laGuqmWfUPc6F7aou66h92mFHPcjCCqsDmbzEGffRhXvHzRLy4Eeb44CJhroEE3aC8LULFYDkb14Wrka2UhfgxJzPOC1PlqQ7Wc",
 })
